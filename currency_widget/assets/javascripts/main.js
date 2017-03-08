2914fed406b6087c8eb0b1e7a0b2da7dcc01193e
n.krikun <n.krikun@mac677rds.local>
2017-03-08 23:40:53 +0300

    Added dropdown functions

--- currency_widget/assets/javascripts/main.js
+++ currency_widget/assets/javascripts/main.js
@@ -4,64 +4,84 @@ import ReactDOM from 'react-dom'
 class Widget extends Component {
   constructor() {
     super()
-    this.model = {
-      input: [{
-        name: 'USD',
-        checked: true
-      }, {
-        name: 'EUR',
-        checked: false
-      }, {
-        name: 'GBP',
-        checked: false
-      }],
-      output: [{
-        name: 'EUR',
-        checked: true
-      }, {
-        name: 'USD',
-        checked: false
-      }, {
-        name: 'GBP',
-        checked: false
-      }]
+    this.state = {
+      input: {
+        opened: true,
+        items:[
+          { id: 1, name: 'USD', checked: true },
+          { id: 2, name: 'EUR', checked: false },
+          { id: 3, name: 'GBP', checked: false }
+        ]
+      },
+      output: {
+        opened: false,
+        items: [
+          { id: 1, name: 'EUR', checked: true },
+          { id: 2, name: 'USD', checked: false },
+          { id: 3, name: 'GBP', checked: false }
+        ]
+      }
     }
   }
 
-  handleCurrencyChange() {
-    const select = event.currentTarget.parentNode;
-    select.classList.add('is-change')
+  sortItems(type) {
+    this.state[type].items.sort((x, y) =>
+      x.checked === y.checked
+        ? 0 
+        : x.checked ? -1 : 1
+    )
   }
 
-  renderCurrency(item) {
-    const click = item.checked
-      ? this.handleCurrencyChange
-      : () => {}
+  toggleList(type) {
+    this.setState(prevState => ({
+      [type]: {
+        opened: !prevState[type].opened,
+        items: prevState[type].items
+      }      
+    }))
+  }
+
+  handleCurrencyChange(type, id) {
+    // this.setState(prevState => ({
+    //   [type]: {
+    //     prevState.opened
+    //   }
+    // }))
+    // this.state[type].items.map(item => item.checked = item.id === id);
+    // this.sortItems(type);
+  }
+
+  renderCurrency(item, type) {
     return (
-      <div className="widget__currency-item" onClick={click}>{item.name}</div>
+      <div className="widget__currency-item"
+        key={item.id}
+        type={type}
+        onClick={() => this.handleCurrencyChange(type, item.id)}>
+        {item.name}
+      </div>
+    )
+  }
+
+  renderPart(data, type) {
+    const field = type === 'input'
+      ? ( <input className="widget__field js-widget-input" type="text" placeholder="Type value" onChange={this.handleAmmountChange} /> )
+      : ( <div className="widget__field js-widget-output"></div> );
+
+    return (
+      <div className="widget__part">
+        <div className={`widget__currency js-widget-currency ${data.opened ? 'is-opened' : ''}`}>
+          {data.items.map(item => this.renderCurrency(item, type) )}
+          <span className="widget__arrow" onClick={() => this.toggleList(type)}></span>
+        </div>
+        {field}
+      </div>
     )
   }
 
   render() {
     return (
       <div className="widget js-widget">
-        <div className="widget__part">
-          <div className="widget__currency js-widget-currency" data-type="input">
-            {this.model.input.map(item => { this.renderCurrency(item) })}
-            <span className="widget__arrow"></span>
-          </div>
-          <input className="widget__field js-widget-input"
-            type="text"
-            placeholder="Type value"
-            onChange={this.handleAmmountChange} />
-        </div>
-        <div className="widget__part">
-          <div className="widget__currency js-widget-currency" data-type="output">
-            {this.model.output.map(item => { this.renderCurrency(item) })}
-            <span className="widget__arrow"></span>
-          </div>
-          <div className="widget__field js-widget-output"></div>
-        </div>
+        {Object.keys(this.state).map(key => this.renderPart(this.state[key], key))}
       </div>
     )
   }
