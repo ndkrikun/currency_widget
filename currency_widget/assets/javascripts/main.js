62aad956889fe9c8287ffaa23c17d3961109f250
n.krikun <n.krikun@minotelte-miphone.rambler.ramblermedia.com>
2017-03-10 11:51:52 +0300

    Removed unnecessary script

--- currency_widget/assets/javascripts/main.js
+++ currency_widget/assets/javascripts/main.js
@@ -96,12 +96,12 @@ class Widget extends Component {
       &&  this.state[key].hasOwnProperty('items')) {
         this.state[key].items.map((item) => {
           if (item.checked) {
-            currency[key] = item.id;
+            currency[key] = item.id
           }
         })
       }
     }
-    return currency;
+    return currency
   }
 
   /**
@@ -128,7 +128,7 @@ class Widget extends Component {
   countResult(val) {
     const inputVal = val === undefined ? this.state.inputVal : val
     const { input, output } = this.getActiveCurrency()
-    let result;
+    let result
     if (input === output) {
       return inputVal
     }
@@ -177,7 +177,7 @@ class Widget extends Component {
    * @param  {Object} event [event context]
    */
   handleAmmountChange(event) {
-    let inputVal = +event.target.value;
+    let inputVal = +event.target.value
     if (!isNaN(inputVal)) {
       this.setState({
         inputVal: inputVal,
@@ -214,14 +214,19 @@ class Widget extends Component {
    */
   renderPart(data, type) {
     const field = type === 'input'
-      ? ( <input className="widget__field js-widget-input" type="text" placeholder="Type value" value={this.state.inputVal} onChange={this.handleAmmountChange.bind(this)} /> )
-      : ( <div className="widget__field js-widget-output">{this.state.outputVal}</div> )
+      ? ( <input className="widget__field" type="text" placeholder="Type value" 
+          value={this.state.inputVal} 
+          onChange={this.handleAmmountChange.bind(this)} />
+        )
+      : ( <div className="widget__field">{this.state.outputVal}</div> )
 
     return (
       <div className="widget__part">
-        <div className={`widget__currency js-widget-currency ${data.opened ? 'is-opened' : ''}`}>
+        <div className={`widget__currency ${data.opened ? 'is-opened' : ''}`}>
           {data.items.map(item => this.renderCurrency(item, type) )}
-          <span className="widget__currency-arrow" onClick={() => this.toggleCurrency(type)}></span>
+          <span className="widget__currency-arrow"
+            onClick={() => this.toggleCurrency(type)}>
+          </span>
         </div>
         {field}
       </div>
@@ -234,7 +239,7 @@ class Widget extends Component {
    */
   render() {
     return (
-      <div className="widget js-widget">
+      <div className="widget">
         {Object.keys((({ input, output }) => ({ input, output }))(this.state)).map(
           key => this.renderPart(this.state[key], key)
         )}
